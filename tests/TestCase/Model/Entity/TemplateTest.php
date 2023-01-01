190adccb548f839b22ab555af4f131a3ae19a2a6
dreamingmind <ddrake@dreamingmind.com>
2023-01-01 10:01:50 -0800

    add two stub tests

---
+++ tests/TestCase/Model/Entity/TemplateTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Test\TestCase\Model\Entity;
+
+class TemplateTest extends \Cake\TestSuite\TestCase
+{
+
+}
---
+++ tests/TestCase/Model/Model/TemplatesTableTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Test\TestCase\Model\Model;
+
+class TemplatesTableTest extends \Cake\TestSuite\TestCase
+{
+    //Should always return with Standards?
+    //configure and test that basic config
+    //returns containment on simple find or get
+
+}
