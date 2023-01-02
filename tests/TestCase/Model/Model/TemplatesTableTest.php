20149ac1cc0b774d4ef0afd0b8516cdba4af22f3
dreamingmind <ddrake@dreamingmind.com>
2023-01-01 19:41:52 -0800

    test that find always contains Standards

--- tests/TestCase/Model/Model/TemplatesTableTest.php
+++ tests/TestCase/Model/Model/TemplatesTableTest.php
@@ -2,10 +2,34 @@
 
 namespace App\Test\TestCase\Model\Model;
 
+use App\Test\Factory\TemplateFactory;
+use App\Test\Traits\RetrievalTrait;
+
 class TemplatesTableTest extends \Cake\TestSuite\TestCase
 {
+
+    use RetrievalTrait;
     //Should always return with Standards?
     //configure and test that basic config
     //returns containment on simple find or get
+
+    public function test_findAutomaticallyContainsStandards()
+    {
+        $factory = TemplateFactory::make()
+            ->listeningToModelEvents(['Model.beforeFind']);
+        $persisted = $factory
+            ->withStandards(5)
+            ->persist();
+
+        $template = $factory->getTable()
+            ->find()
+            ->where(['id' => $persisted->id])
+            ->toArray();
+
+        $this->assertCount(
+            count($this->getRecords('Standards')),
+            $template[0]->standards
+        );
+    }
 
 }
