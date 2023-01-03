2262aed6ae10ecd5f03611720b3d7ac1c7728f7e
Jason Tempestini <jason@curlymedia.com>
2023-01-02 16:18:32 -0800

    save testing sequence

--- tests/TestCase/Model/Table/TemplatesTableTest.php
+++ tests/TestCase/Model/Table/TemplatesTableTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Test\TestCase\Model\Table;
 
+use App\Test\Factory\DepartmentFactory;
 use App\Test\Factory\TemplateFactory;
 use App\Test\Traits\RetrievalTrait;
 use Cake\TestSuite\TestCase;
@@ -41,16 +42,59 @@ class TemplatesTableTest extends TestCase
         $template = $factory->getTable()
             ->get($persisted->id);
 
+        debug($template->toArray());
+
         $this->assertCount(
             count($this->getRecords('Standards')),
             $template->standards
         );
     }
 
-    public function test_StandardsContainedWhenTemplatesContained()
+    public function test_saveStandardsOnTemplateWithSequence()
     {
+        $department = DepartmentFactory::make()->persist();
+        $patch = [
+            'name' => 'template name',
+            'standards' => [
+                (int) 0 => [
+                    'process_code' => '1234',
+                    'name' => 'prepress',
+                    'uom' => 'min',
+                    'units_per_hour' => (float) 60,
+                    'daily_capacity' => 480,
+                    '_joinData' => [
+                        'sequence' => 0
+                    ],
+                    'department_id' => $department->id
+                ],
+                (int) 1 => [
+                    'process_code' => '1234',
+                    'name' => 'prepress',
+                    'uom' => 'min',
+                    'units_per_hour' => (float) 60,
+                    'daily_capacity' => 480,
+                    '_joinData' => [
+                        'sequence' => 1
+                    ],
+                    'department_id' => $department->id
+                ]
+            ]
+        ];
+
+        $Template = TemplateFactory::make()->getTable();
+        $entity = $Template->newEntity($patch);
+        $Template->save($entity);
+
+        $records = $this->getRecords('Templates');
+
+        $this->assertCount(1, $records);
+        $this->assertEquals(1, $records[0]->standards[1]->_joinData->sequence);
+        $this->assertEquals(0, $records[0]->standards[0]->_joinData->sequence);
 
     }
+
+
+
 
 }
 
