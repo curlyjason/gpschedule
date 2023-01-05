135b882d299feb6528a1a120ce12c2beb17fd8d0
dreamingmind <ddrake@dreamingmind.com>
2023-01-04 20:16:58 -0800

    build up a few ideas in ProcessSet

--- src/Utilities/ProcessSet.php
+++ src/Utilities/ProcessSet.php
@@ -4,9 +4,47 @@ namespace App\Utilities;
 
 class ProcessSet
 {
+    private array $keydById;
+    private array $keyedByPrereq;
+
     public function __construct(array $processes)
     {
+        $this->keydById = collection($processes)
+            ->indexBy(function($process) {
+                return $process->id;
+            })
+            ->toArray();
+        $this->buildLookup();
+        debug($this->keyedByPrereq);
+    }
 
+    private function buildLookup()
+    {
+        $keys = array_keys($this->keydById);
+        $this->keyedByPrereq = array_fill_keys($keys, []);
+        collection($keys)
+            ->each(function($key) {
+                $this->keyedByPrereq[$this->getPrereqOf($key)][] = $key;
+            })
+            ->toArray();
+    }
+
+    /**
+     * @param $key
+     * @return mixed
+     */
+    public function getPrereqOf($key)
+    {
+        return $this->keydById[$key]->prereq;
+    }
+
+    /**
+     * @param $key
+     * @return mixed
+     */
+    public function getFollowersOf($key)
+    {
+        return $this->keyedByPrereq[$key]->prereq;
     }
 
 }
