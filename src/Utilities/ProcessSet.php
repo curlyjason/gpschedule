da0d733c5928e617731353bb694b789f54062582
dreamingmind <ddrake@dreamingmind.com>
2023-01-04 22:16:17 -0800

    sketch in some class proposals

--- src/Utilities/ProcessSet.php
+++ src/Utilities/ProcessSet.php
@@ -2,11 +2,17 @@
 
 namespace App\Utilities;
 
+use App\Model\Entity\Process;
+
 class ProcessSet
 {
     private array $keydById;
     private array $keyedByPrereq;
+    private ?array $iteratorSeed = null;
 
+    /**
+     * @param Process[] $processes
+     */
     public function __construct(array $processes)
     {
         $this->keydById = collection($processes)
@@ -14,19 +20,9 @@ class ProcessSet
                 return $process->id;
             })
             ->toArray();
-        $this->buildLookup();
+        $this->buildFollowerLookup();
+        $this->childIteratorSeed();
         debug($this->keyedByPrereq);
-    }
-
-    private function buildLookup()
-    {
-        $keys = array_keys($this->keydById);
-        $this->keyedByPrereq = array_fill_keys($keys, []);
-        collection($keys)
-            ->each(function($key) {
-                $this->keyedByPrereq[$this->getPrereqOf($key)][] = $key;
-            })
-            ->toArray();
     }
 
     /**
@@ -45,6 +41,57 @@ class ProcessSet
     public function getFollowersOf($key)
     {
         return $this->keyedByPrereq[$key]->prereq;
+    }
+
+    public function getProcess($key)
+    {
+        return $this->keydById[$key] ?? null;
+    }
+
+    public function getChildIterator()
+    {
+        return new \RecursiveArrayIterator($this->childIteratorSeed());
+    }
+
+    private function getInitialProcesses()
+    {
+        $starts = $this->getFollowersOf('');
+        return collection($starts)
+            ->map(function($start) {
+                return $this->getProcess($start);
+            })
+            ->toArray();
+    }
+
+    private function buildFollowerLookup()
+    {
+        $keys = array_keys($this->keydById);
+        $this->keyedByPrereq = array_fill_keys($keys, []);
+        collection($keys)
+            ->each(function($key) {
+                $this->keyedByPrereq[$this->getPrereqOf($key)][] = $key;
+            })
+            ->toArray();
+    }
+
+    private function childIteratorSeed()
+    {
+        if (is_null($this->iteratorSeed)) {
+            $this->iteratorSeed = collection($this->keyedByPrereq)
+                ->reduce(function($accum, $followers, $prereq) {
+                    if (count($followers) < 2) {
+                        $accum[$prereq] = $this->getProcess($followers[0] ?? null);
+                    }
+                    else {
+                        $accum[$prereq] = collection($followers)
+                            ->map(function($follower) {
+                                return $this->getProcess($follower);
+                            })
+                            ->toArray();
+                    }
+                }, []);
+        }
+        return $this->iteratorSeed;
     }
 
 }
