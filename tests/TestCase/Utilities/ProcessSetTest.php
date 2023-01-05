c13c3feff1aac4de74c20eeed06490369d5c1ded
Jason Tempestini <jason@curlymedia.com>
2023-01-05 13:47:03 -0800

    test data

--- tests/TestCase/Utilities/ProcessSetTest.php
+++ tests/TestCase/Utilities/ProcessSetTest.php
@@ -18,63 +18,102 @@ class ProcessSetTest extends TestCase
         $this->loadFixtureScenario('SingleStreamProcess');
         $SetManager = new ProcessSetDouble($this->getRecords('Processes'));
 
-        /**
-         *
-         */
-        $lookup = [
-            '' => ['a', 'b'],
-            'a' => [],
-            'b' => [],
-        ];
-        $result = [
-            ['a'],
-            ['b'],
-        ];
+    }
 
-        /**
-         *
-         */
-        $lookup = [
-            '' => ['a'],
-            'a' => [],
-        ];
-        $result = [
-            'a',
-        ];
+    /**
+     * @param $prereqLookup
+     * @param $expectedResult
+     * @dataProvider process_sorter_provider
+     * @return void
+     */
+    public function test_process_sorter($prereqLookup, $expectedResult)
+    {
+        $SetManager = new ProcessSetDouble([]);
+        $SetManager->setKeyedByPrereq($prereqLookup);
+        debug($SetManager);
+        $result = $SetManager->childIteratorSeed($SetManager->getFollowersOf(''));
+        debug($result);
+    }
 
-        /**
-         *
-         */
-        $lookup = [
-            '' => ['a', 'b'],
-            'a' => ['a.a'],
-            'a.a' => ['a.b'],
-            'a.b' => [],
-            'b' => ['b.a'],
-            'b.a' => [],
+    public function process_sorter_provider()
+    {
+        return [
+//            [
+//                /**
+//                 *two single starts with no following steps
+//                 */
+//                'prereqLookup' => [
+//                    '' => ['a', 'b'],
+//                    'a' => [],
+//                    'b' => [],
+//                ],
+//                'expectedResult' => [
+//                    ['a'],
+//                    ['b'],
+//                ]
+//            ],
+//            [
+//                /**
+//                 *single starts with no following steps
+//                 */
+//                'prereqLookup' => [
+//                    '' => ['a'],
+//                    'a' => [],
+//                ],
+//                'expectedResult' => [
+//                    'a',
+//                ]
+//            ],
+            [
+                /**
+                 *straight line list
+                 */
+                'prereqLookup' => [
+                    '' => ['a'],
+                    'a' => ['a.b'],
+                    'a.b' => ['b.c'],
+                    'b.c' => []
+                ],
+                'expectedResult' => [
+                    'a', 'a.b', 'b.c'
+                ]
+            ],
+//            [
+//                /**
+//                 *single split
+//                 */
+//                'prereqLookup' => [
+//                    '' => ['a', 'b'],
+//                    'a' => ['a.a'],
+//                    'a.a' => ['a.b'],
+//                    'a.b' => [],
+//                    'b' => ['b.a'],
+//                    'b.a' => [],
+//                ],
+//                'expectedResult' => [
+//                    ['a', 'a.a', 'a.b'],
+//                    ['b', 'b.a'],
+//                ]
+//            ],
+//            [
+//                /**
+//                 *double split
+//                 */
+//                'prereqLookup' => [
+//                    '' => ['a', 'b'],
+//                    'a' => [],
+//                    'b' => ['b.a'],
+//                    'b.a' => ['b.a.a', 'b.a.b'],
+//                    'b.a.a' => [],
+//                    'b.a.b' => ['b.a.c'],
+//                    'b.a.c' => []
+//                ],
+//                'expectedResult' => [
+//                    ['a', 'a.a', 'a.b'],
+//                    ['b', 'b.a', ['b.a.a'], ['b.a.b', 'b.a.c']],
+//                ]
+//            ],
         ];
-        $result = [
-            ['a', 'a.a', 'a.b'],
-            ['b', 'b.a'],
-        ];
-
-        /**
-         *
-         */
-        $lookup = [
-            '' => ['a', 'b'],
-            'a' => [],
-            'b' => ['b.a'],
-            'b.a' => ['b.a.a', 'b.a.b'],
-            'b.a.a' => [],
-            'b.a.b' => ['b.a.c'],
-            'b.a.c' => []
-        ];
-        $result = [
-            ['a', 'a.a', 'a.b'],
-            ['b', 'b.a', ['b.a.a'], ['b.a.b', 'b.a.c']],
-        ];
-
     }
 
 }
