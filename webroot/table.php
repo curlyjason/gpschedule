a81c654388e12e5deb0982f36321b753960e1539
dreamingmind <ddrake@dreamingmind.com>
2023-01-18 14:55:28 -0800

    table example

---
+++ webroot/table.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Table</title>
+</head>
+<body>
+<table>
+    <tr>
+        <td colspan="3">word</td>
+        <td>word</td>
+    </tr>
+    <tr>
+        <td colspan="3">word</td>
+        <td>word</td>
+    </tr>
+    <tr>
+        <td colspan="3">word</td>
+        <td>word</td>
+    </tr>
+    <tr>
+        <td colspan="3">word</td>
+        <td>word</td>
+    </tr>
+    <tr>
+        <td colspan="3">word</td>
+        <td>word</td>
+    </tr>
+    <tr>
+        <td>word</td>
+        <td colspan="2">word</td>
+        <td>word</td>
+    </tr>
+    <tr>
+        <td>word</td>
+        <td colspan="2">word</td>
+        <td>word</td>
+    </tr>
+    <tr>
+        <td>word</td>
+        <td colspan="2">word</td>
+        <td>word</td>
+    </tr>
+    <tr>
+        <td>word</td>
+        <td colspan="2">word</td>
+        <td>word</td>
+    </tr>
+    <tr>
+        <td>word</td>
+        <td colspan="2">word</td>
+        <td>word</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>word</td>
+        <td>word</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>word</td>
+        <td>word</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>word</td>
+        <td>word</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>word</td>
+        <td>word</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>word</td>
+        <td>word</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td>word</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td>word</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td>word</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td>word</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td>word</td>
+        <td></td>
+    </tr>
+</table>
+</body>
+</html>
